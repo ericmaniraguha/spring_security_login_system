b6921a32272f8b98f860ab7d95bd625d34aa4ee7
ericmaniraguha <ericmaniraguha@gmail.com>
2023-07-30 10:37:41 +0200

    create UserService and SecurityConfigurations

--- src/main/java/com/springbootsecurity/configuration/SecurityConfiguration.java
+++ src/main/java/com/springbootsecurity/configuration/SecurityConfiguration.java
@@ -1,20 +1,48 @@
+// Import necessary classes and interfaces
 package com.springbootsecurity.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+
+// Define the SecurityConfiguration class as a configuration class
 @Configuration
 public class SecurityConfiguration {
 	
-	@Bean
-   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    // Create a bean for the PasswordEncoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // Return a new instance of BCryptPasswordEncoder
+        return new BCryptPasswordEncoder();
+    }
+  
+    // Create a bean for the AuthenticationManager
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService detailsService) {
+        // Create a new instance of DaoAuthenticationProvider
+        DaoAuthenticationProvider daoProvider = new DaoAuthenticationProvider();
+        // Set the UserDetailService for the DaoAuthenticationProvider
+        daoProvider.setUserDetailsService(detailsService);
+        // Return a new instance of ProviderManager with the DaoAuthenticationProvider
+        return new ProviderManager(daoProvider);
+    }
 
-	   return http
-			   .csrf(csrf -> csrf.disable())
-			   .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-			   .build();
-		
-   }
+    // Create a bean for the SecurityFilterChain
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Configure HttpSecurity to disable CSRF protection and permit all requests
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic().and()
+                .build();
+    }
 }
